1e3606b20c04b61bb08fba88076eef0ae201afb0
Starpia <starpia.forge@gmail.com>
2025-08-12 15:42:27 +0900

    Add `implicit.go` example to demonstrate implicit schema unmarshaling

---
+++ cmd/example/implicit/implicit.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/starpia-forge/be-schema"
+)
+
+func main() {
+	data := []byte("89\r\n[[\"test1\",\"test2\",\"[[null]]\",null,null,null,\"test3\"],[\"test4\",1],[\"test5\",2,\"test6\",3]]\r\n")
+
+	s, err := beschema.UnmarshalImplicitSchema(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%+v\n", s)
+}
