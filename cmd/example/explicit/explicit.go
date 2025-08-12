87cd0dfef81828b6c428a7606cf4dc5b404952ac
Starpia <starpia.forge@gmail.com>
2025-08-12 17:03:04 +0900

    Update variable names in `explicit.go` and `implicit.go` for clarity

--- cmd/example/explicit/explicit.go
+++ cmd/example/explicit/explicit.go
@@ -24,9 +24,9 @@ type SubEntity2 struct {
 func main() {
 	data := []byte("89\r\n[[\"test1\",\"test2\",\"[[null]]\",null,null,null,\"test3\"],[\"test4\",1],[\"test5\",2,\"test6\",3]]\r\n")
 
-	f, err := beschema.UnmarshalExplicitSchema[Entity](data)
+	entity, err := beschema.UnmarshalExplicitSchema[Entity](data)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Printf("%+v\n", f)
+	log.Printf("%+v\n", entity)
 }
--- cmd/example/implicit/implicit.go
+++ cmd/example/implicit/implicit.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	data := []byte("89\r\n[[\"test1\",\"test2\",\"[[null]]\",null,null,null,\"test3\"],[\"test4\",1],[\"test5\",2,\"test6\",3]]\r\n")
 
-	s, err := beschema.UnmarshalImplicitSchema(data)
+	entity, err := beschema.UnmarshalImplicitSchema(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("%+v\n", s)
+	log.Printf("%+v\n", entity)
 }
